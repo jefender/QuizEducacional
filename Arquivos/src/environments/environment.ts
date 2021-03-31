39f8ac737a58663fce380f877e6cbc52b85c190a
Jeferson Ferreira <jefender@gmail.com>
2021-03-31 20:16:10 -0300

    .gitignore

--- Arquivos/src/environments/environment.ts
+++ Arquivos/src/environments/environment.ts
@@ -10,6 +10,7 @@ export const environment = {
 };
 
 /*
+	Atualizado
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
